4998c3898af886573d91cccf74ef0b47cf47e0c5
xxy <xxy@xiangyang.com>
2017-12-04 13:41:42 +0800

    fix auto redirect to https

--- lib/rails_slb_middleware.rb
+++ lib/rails_slb_middleware.rb
@@ -10,12 +10,12 @@ class RailsSlbMiddleware
   def call(env)
     req = Rack::Request.new(env)
 
-    if @force_rewrite_https && req.get_header("HTTP_X_FORWARDED_PROTO") == "https" && req.scheme == "http"
-      return [301, {"Location" => req.url.gsub(/^http/, "https")}, []]
-    end
-
     if req.path == "/ping"
       return [200, {"Content-Type" => "text/plain"}, ["PONG"]]
+    end
+
+    if @force_rewrite_https && req.get_header("HTTP_X_FORWARDED_PROTO") == "http"
+      return [301, {"Location" => req.url.gsub(/^http/, "https"), "Strict-Transport-Security" => "max-age=31536000"}, []]
     end
 
     @app.call(req.env)
--- lib/rails_slb_middleware/version.rb
+++ lib/rails_slb_middleware/version.rb
@@ -1,3 +1,3 @@
 class RailsSlbMiddleware
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
